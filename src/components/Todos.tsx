ba7b7be9ab4befa56a7759b7f51da6833fe9e5c2
paulbkim1 <paulbkim1@gmail.com>
2024-03-11 15:02:05 -0700

    typescript-demo

---
+++ src/components/Todos.tsx
@@ -0,0 +1,21 @@
+import Todo from "../models/todo";
+import TodoList from "./TodoList";
+import classes from "./TodoList.module.css";
+
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
+  return (
+    <ul className={classes.todos}>
+      {props.items.map((item) => (
+        <TodoList
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
